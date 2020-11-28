19bd0cda35f430987a839c68f1834f2b568a0064
Francisco Vaz <ftomasvaz@gmail.com>
2020-11-28 17:11:03 +0000

    rpad implemented

--- src/app/pipes/rpad.pipe.ts
+++ src/app/pipes/rpad.pipe.ts
@@ -5,8 +5,26 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class RpadPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
-  }
+  transform(rpad: string, numberOfRPads?: number, simbol?: string): string {
+    if(!rpad) {
+      return 'Inválido';
+    }
 
+    let numberOfChars: number = rpad.length;
+    let stringToReturn: string = '';
+
+    if(numberOfRPads) {
+      let numberOfSimbolsToAdd = numberOfRPads - numberOfChars;
+      for(let i=0; i < numberOfSimbolsToAdd; i++){
+        if(simbol){
+          stringToReturn+=`${simbol}`;
+        }else {
+          stringToReturn+=`*`;
+        }
+      }
+      return `${rpad}${stringToReturn}`
+    } else {
+      return `${rpad}*****`;
+    }
+  }
 }
